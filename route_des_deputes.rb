3dc421be97ec2514fb85b6e37c9feb24d2b22cc2
Ravencod <42932340+Ravencod@users.noreply.github.com>
2018-10-11 21:29:07 +0200

    modif

--- route_des_deputes.rb
+++ route_des_deputes.rb
@@ -3,25 +3,21 @@ require 'nokogiri'
 require 'open-uri'
 
 def route_des_deputes
-start = Time.now
 page1 = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/regions"))
-array = []
+deputes = []
 x = 0
 while x < page1.xpath("//*[@id='deputes-list']/div/ul/li/a").size
-    a = page1.xpath("//*[@id='deputes-list']/div/ul/li/a")[x].text
+    a = page1.xpath("//*[@id='deputes-list']/div/ul/li/a")[x].text.encode!
     z = page1.xpath("//*[@id='deputes-list']/div/ul/li/a")[x]['href']
     page2 = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr#{z}"))
-    b = page2.xpath("/html/body/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li/a")[0]['href'].delete_prefix('mailto:')
-    array.push({a => b})
-    puts a
-    puts b
+    (page2.xpath("/html/body/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li/a").empty?) ? "no mail" : b = page2.xpath("/html/body/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li/a")[0]['href'].delete_prefix('mailto:')
+    deputes.push({:first_name => a.split(' ')[1] , :last_name => a.split(' ')[2], :email => b})
+    
     x+=1
 end
-puts array
-finish = Time.now
-p diff = finish-start
+    puts deputes
 end
-
+t_start = Time.now
 route_des_deputes
-
-
+t_end = Time.now
+puts "\nThis took #{Time.at(t_end - t_start).utc.strftime("%H:%M:%S")} to end"
